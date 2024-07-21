ee186e4329ac0d2b5a15748389c1c8f9fa85fd84
Krishna Pathak <pathakkare@gmail.com>
2024-07-21 16:48:08 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors(
     {
-        origin: ['http://localhost:5173', 'https://artfy-frontend-un7fz0uje-krishnajis-projects.vercel.app', 'https://artfy-frontend.vercel.app']
+        origin: ['http://localhost:5173','https://artify-frontend.netlify.app', 'https://artfy-frontend-un7fz0uje-krishnajis-projects.vercel.app', 'https://artfy-frontend.vercel.app']
     }
 ));
 
